44e54dbaa790c20eb524d7dcfb253f4c11147636
Day-Go <lucarusso.521@gmail.com>
2025-09-09 23:18:29 +1000

    correct storage spacing

--- storage.c
+++ storage.c
@@ -12,12 +12,15 @@ StorageArea storage_area_create(Vector2 start_pos, int n_cols, int n_rows,
   storage_area.shelves = malloc(n_cols * n_rows * n_lvls * sizeof(Shelf));
 
   for (int lvl = 0; lvl < n_lvls; lvl++) {
+    float x = start_pos.x;
     for (int col = 0; col < n_cols; col++) {
+      x += SHELF_SIZE + COL_SPACING;
+      if (col % 2 == 1) {
+        x += COL_GAP;
+      }
       for (int row = 0; row < n_rows; row++) {
-        float x = start_pos.x + SHELF_SIZE * col + COL_SPACING * col;
         float y = start_pos.y + SHELF_SIZE * row + ROW_SPACING * row;
-        printf("%i, %i, %i\n", lvl, col, row);
-        printf("%f, %f\n\n", x, y);
+
         int idx = lvl * (n_rows * n_cols) + row * n_cols + col;
         storage_area.shelves[idx] = (Shelf){.bin = "A",
                                             .shape = {.x = x,
@@ -33,6 +36,8 @@ StorageArea storage_area_create(Vector2 start_pos, int n_cols, int n_rows,
   return storage_area;
 }
 
+void storage_area_destroy(StorageArea storage_area) {}
+
 void storage_shelves_draw(StorageArea *storage_area, int lvl) {
   for (int col = 0; col < storage_area->n_cols; col++) {
     for (int row = 0; row < storage_area->n_rows; row++) {
@@ -47,4 +52,23 @@ void storage_shelves_draw(StorageArea *storage_area, int lvl) {
   }
 }
 
-void storage_area_destroy(StorageArea storage_area) {}
+void storage_shelf_highlight(Shelf *shelf) {
+  DrawRectangleRec(shelf->shape, GREEN);
+}
+
+Shelf *storage_area_mouse_entered(StorageArea *storage_area,
+                                  Vector2 mouse_pos) {
+  for (int lvl = 0; lvl < storage_area->n_lvls; lvl++) {
+    for (int col = 0; col < storage_area->n_cols; col++) {
+      for (int row = 0; row < storage_area->n_rows; row++) {
+        int idx = lvl * (storage_area->n_rows * storage_area->n_cols) +
+                  row * storage_area->n_cols + col;
+        if (CheckCollisionPointRec(mouse_pos,
+                                   storage_area->shelves[idx].shape)) {
+          return &storage_area->shelves[idx];
+        }
+      }
+    }
+  }
+  return NULL;
+}
--- storage.h
+++ storage.h
@@ -1,7 +1,8 @@
 #include "raylib.h"
 
 #define SHELF_SIZE 25
-#define COL_SPACING 60
+#define COL_GAP 50
+#define COL_SPACING 5
 #define ROW_SPACING 5
 
 typedef struct {
@@ -20,5 +21,8 @@ typedef struct {
 
 StorageArea storage_area_create(Vector2 start_pos, int n_cols, int n_rows,
                                 int n_lvls);
+
 void storage_area_destroy(StorageArea storage_area);
+Shelf *storage_area_mouse_entered(StorageArea *storage_area, Vector2 mouse_pos);
 void storage_shelves_draw(StorageArea *storage_area, int lvl);
+void storage_shelf_highlight(Shelf *shelf);
